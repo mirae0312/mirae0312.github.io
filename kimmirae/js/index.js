96ecb25634f1abe4d681fede432220d2bcf389d6
Mirae Kim <77021071+mirae0312@users.noreply.github.com>
2023-11-28 21:00:26 +0900

    Delete kimmirae directory

--- kimmirae/js/index.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector('.navbar_toggleBtn');
-const menu = document.querySelector('.navbar_menu');
-
-// 메뉴버튼 click
-btn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
